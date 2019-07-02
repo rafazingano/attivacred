34f9c1d0bc9c46c938c99e0501d1b4032b54fb05
Rafael Zingano <confrariaweb@gmail.com>
2019-07-01 22:37:25 -0300

    Update all

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        DB::table('taxas')->insert([
+            'imovel' => 1.4,
+            'carro' => 20,
+            'pessoal' => 30,
+        ]);
     }
 }
