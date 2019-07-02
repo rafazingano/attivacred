40dfb06302ba2cb67e2b80663e2743459030e33a
Rafael Zingano <confrariaweb@gmail.com>
2019-07-01 22:50:52 -0300

    Update all

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -36,8 +36,7 @@
 										<i class="fa fa-home"></i>
 									</a>
 								</li>
-								<li><span>Layouts</span></li>
-								<li><span>Default</span></li>
+								<li><span>Dashboard</span></li>
 							</ol>
 							<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
 						</div>
